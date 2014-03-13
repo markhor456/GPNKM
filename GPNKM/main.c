eab3fec519e4dd49fd7bd3e0f41916dcfaaeca50
antoine <antoine@antoine-netbook>
2014-03-13 16:00:02 +0100

    First commit!

---
+++ GPNKM/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: antoine
+ *
+ * Created on 13 mars 2014, 15:58
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello World!\n");
+    return (EXIT_SUCCESS);
+}
+
